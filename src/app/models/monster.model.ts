7a698aec23bb64b8756c1772d65112142014963a
Camel Djoulako <cameldjoulako17@gmail.com>
2024-10-04 12:40:25 +0100

    service in angular

--- src/app/models/monster.model.ts
+++ src/app/models/monster.model.ts
@@ -1,6 +1,7 @@
 import { MonsterType } from '../utils/monster.utils';
 
 export class Monster {
+  id: number = -1;
   name: string = 'My Monster';
   image: string = 'assets/img/play.jpg';
   type: MonsterType = MonsterType.ELECTRIC;
