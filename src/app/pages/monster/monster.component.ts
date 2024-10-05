7fb190dd0f81064bf7ff5ecde25228cd58c0fe92
Camel Djoulako <cameldjoulako17@gmail.com>
2024-10-05 13:53:25 +0100

    reactif form with formcontrol formgroup and form builder

--- src/app/pages/monster/monster.component.ts
+++ src/app/pages/monster/monster.component.ts
@@ -1,21 +1,74 @@
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { MonsterType } from '../../utils/monster.utils';
 
 @Component({
   selector: 'app-monster',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './monster.component.html',
   styleUrl: './monster.component.css',
 })
 export class MonsterComponent implements OnInit, OnDestroy {
   private route = inject(ActivatedRoute);
-
   private router = inject(Router);
+
+  private formBuilder = inject(FormBuilder);
+
+  //gestion des erreurs avec formcontrol
+  /* name = new FormControl('', [Validators.required]);
+  hp = new FormControl('', [
+    Validators.required,
+    Validators.min(0),
+    Validators.max(200),
+  ]); */
+
+  //gestion des erreurs avec formGroup
+  /* formGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required]),
+    type: new FormControl(MonsterType.ELECTRIC, [Validators.required]),
+    hp: new FormControl(0, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(200),
+    ]),
+    figureCaption: new FormControl('', [Validators.required]),
+    attackName: new FormControl('', [Validators.required]),
+    attackStrength: new FormControl(0, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(200),
+    ]),
+    attackDescription: new FormControl('', [Validators.required]),
+  }); */
+
+  //gestion des erreurs avec FormBuilder
+  formGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    image: ['', [Validators.required]],
+    type: [MonsterType.ELECTRIC, [Validators.required]],
+    hp: [0, [Validators.required, Validators.min(1), Validators.max(200)]],
+    figureCaption: ['', [Validators.required]],
+    attackName: ['', [Validators.required]],
+    attackStrength: [
+      0,
+      [Validators.required, Validators.min(1), Validators.max(200)],
+    ],
+    attackDescription: ['', [Validators.required]],
+  });
 
   routeSubscription: Subscription | null = null;
 
+  monsterTypes = Object.values(MonsterType);
   monsterId = signal<number | undefined>(undefined);
 
   ngOnInit(): void {
@@ -33,5 +86,33 @@ export class MonsterComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
+  }
+
+  submit(event: Event) {
+    event.preventDefault();
+    console.log(this.formGroup.value);
+  }
+
+  /*  setChanged() {
+    this.name.setValue('Changed');
+  } */
+
+  isFieldValid(name: string) {
+    const formControl = this.formGroup.get(name);
+
+    return formControl?.invalid && (formControl?.dirty || formControl?.touched);
+  }
+
+  onFileChange(event: any) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.formGroup.patchValue({
+          image: reader.result as string,
+        });
+      };
+    }
   }
 }
